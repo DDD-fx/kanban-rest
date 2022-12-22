1066916a66dd47c43ef0dabe2411c45a58fe0ff8
ddd-fx <d.khoroshkin@gmail.com>
2022-12-22 23:50:50 +0600

    update ormconfig

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -9,6 +9,17 @@ dotenv.config({
 const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST } = process.env;
 const LOCAL_URL = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`;
 
+export default {
+  type: 'postgres',
+  cache: false,
+  url: (process.env.DATABASE_URL as string) || LOCAL_URL,
+  synchronize: false,
+  logging: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+} as DataSourceOptions;
+
 const connectionSource = new DataSource({
   type: 'postgres',
   cache: false,
@@ -25,14 +36,3 @@ const connectionSource = new DataSource({
 (async function run() {
   await connectionSource.initialize();
 })();
-
-export default {
-  type: 'postgres',
-  cache: false,
-  url: (process.env.DATABASE_URL as string) || LOCAL_URL,
-  synchronize: false,
-  logging: false,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-} as DataSourceOptions;
