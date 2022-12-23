014a243b6ebd954e1ca734acd1f15e04a2190d4c
ddd-fx <d.khoroshkin@gmail.com>
2022-12-23 11:08:41 +0600

    run migrationa manually

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -9,21 +9,21 @@ dotenv.config({
 const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST } = process.env;
 const LOCAL_URL = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`;
 
-export default {
-  type: 'postgres',
-  cache: false,
-  url: (process.env.DATABASE_URL as string) || LOCAL_URL,
-  synchronize: false,
-  logging: false,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-} as DataSourceOptions;
+// export default {
+//   type: 'postgres',
+//   cache: false,
+//   url: (process.env.DATABASE_URL as string) || LOCAL_URL,
+//   synchronize: false,
+//   logging: false,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// } as DataSourceOptions;
 
-const connectionSource = new DataSource({
+export const connectionSource = new DataSource({
   type: 'postgres',
   cache: false,
-  url: (process.env.DATABASE_URL as string) || LOCAL_URL,
+  url: 'postgres://kanban_api_postgre_user:8iL6CfF1g43isE2tkSNsCUcLAzpjNNjW@dpg-cdnnq1irrk05dt1o4uc0-a.frankfurt-postgres.render.com/kanban_api_postgre',
   synchronize: false,
   logging: false,
   ssl: {
